b2a52a607ee493c2949977124cabdded6b09232c
cptnattica <cptn_attica2@yahoo.com>
2026-09-10 22:26:15 +0800

    added dashboard and my tasks page.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/tasks', function () {
+    return view('task');
+})->name('task');
